114f8926db7b77115df19e9859e5030e9102ae66
raju <raju@zessta.com>
2019-01-09 18:35:59 +0530

    changes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 const db = {};
 
 const sequelize=new Sequelize(process.env.DB_NAME||'testDemo',process.env.DB_USER||'root',process.env.DB_PASS||'zessta',
@@ -15,6 +16,14 @@ fs.readdirSync(__dirname)
         const model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model;
     });
+mongodb.connect('mongodb://localhost:27017/mydb',(err,dbclient)=>{
+    if (err) throw err;
+    console.log("Database created!");
+    dbclient.db('mydb1').createCollection('users',(err,collection)=>{if (err) throw err;
+        console.log("collection created!");
+        //console.log(collection);
+    })
+})
 db.sequelize=sequelize;
 db.Sequelize=Sequelize;
 module.exports=db;
